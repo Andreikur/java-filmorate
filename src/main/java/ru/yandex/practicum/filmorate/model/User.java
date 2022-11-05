7a77dc70a183b50299b5f73ada9614f8c4ca2661
Andreikur <andreikur1@yandex.ru>
2022-11-05 22:56:23 +0300

    Спринт 9 На проверку01.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
@@ -8,10 +7,10 @@ import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
-@Builder
 public class User {
 
     private int id;
+
     @NonNull
     @NotEmpty
     @Email(regexp = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}", message="Пожалуйста укажите дейтвительный адрес")
